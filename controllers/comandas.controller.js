30622c5d18433cfb1c47abd37bb673838fdb6258
elvinpoma <elvin.isai.port.mart@gmail.com>
2026-03-23 13:04:29 +0100

    Datos de comanda. Mal hechos...

--- controllers/comandas.controller.js
+++ controllers/comandas.controller.js
@@ -1,7 +1,9 @@
 import * as camisetasService from '../services/comandas.services.js';
 
 export function crearComanda(req, res) {
-  const filtros = req.body
-  const resultado = camisetasService.crearComanda(filtros);
+  const body = req.body
+  const { cliente, direccion, items } = req.body;
+  const resultado = camisetasService.validarDatos(cliente,direccion,items);
+
   res.status(200).json(resultado);
 }
--- services/comandas.services.js
+++ services/comandas.services.js
@@ -1,13 +1,19 @@
 import { comandas } from "../data/comandas.js";
 
 
-
-export function crearComanda(datos) {
-  const resultado = comandas
+export function crearComanda() {
+    const resultado = comandas
     comprobarDatos(datos)
-  return resultado;
+    return resultado;
 }
 
-function comprobarDatos(){
+export function validarDatos(cliente, direccion, items) {
+    const respuesta = true
+    if (cliente.nombre === null || cliente.email === null) { respuesta = false }
+    if (direccion.calle === null || direccion.cp === null || direccion.ciudad === null) { respuesta = false }
 
+        items => {
+            item.camisetaId === null || item.talla === null|| item.color === null || item.cantidad == null
+        }
+    
 }
